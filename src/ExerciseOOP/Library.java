1193a3071ffcd73d691e7e26c9af877896fc2add
Van Vo <vanminho296@gmail.com>
2023-06-15 14:47:16 +0700

    Changing some code as thinhkhang97 comment.

--- src/ExerciseOOP/Library.java
+++ src/ExerciseOOP/Library.java
@@ -7,20 +7,8 @@ import java.util.Scanner;
 public class Library {
 
     public static void main(String[] args) {
-        List<Book> listBook = new ArrayList<>();
-        Book book1 = new Book("Doanh Nghiep The Ki 21", "Robert", "1/1/2000", "Available");
-        Book book2 = new Book("NDCD", "Bob Burg", "1/1/2000", "Available");
-        Book book3 = new Book("NLDKCD", "Robin Sharma", "1/1/2000", "Available");
-        listBook.add(book1);
-        listBook.add(book2);
-        listBook.add(book3);
-        System.out.println("---------MENU------------");
-        System.out.println("1. Add book");
-        System.out.println("2.Search book");
-        System.out.println("3. Borrow book");
-        System.out.println("4. Return book");
-        System.out.println("5. Display book");
-        System.out.println("6. Exit");
+        List<Book> listBook = initBooks();
+        showMenu();
         Scanner scanner = new Scanner(System.in);
         System.out.println("What do you want?. Please type the number as the menu");
         int userType = scanner.nextInt();
@@ -44,6 +32,25 @@ public class Library {
         }
     }
 
+    public static void showMenu(){
+        System.out.println("---------MENU------------");
+        System.out.println("1. Add book");
+        System.out.println("2.Search book");
+        System.out.println("3. Borrow book");
+        System.out.println("4. Return book");
+        System.out.println("5. Display book");
+        System.out.println("6. Exit");
+    }
+    public static List<Book> initBooks(){
+        List<Book> listBook = new ArrayList<>();
+        Book book1 = new Book("Doanh Nghiep The Ki 21", "Robert", "1/1/2000", "Available");
+        Book book2 = new Book("NDCD", "Bob Burg", "1/1/2000", "Available");
+        Book book3 = new Book("NLDKCD", "Robin Sharma", "1/1/2000", "Available");
+        listBook.add(book1);
+        listBook.add(book2);
+        listBook.add(book3);
+        return listBook;
+    }
     public static List<Book> addBook() {
         List<Book> listBook = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
