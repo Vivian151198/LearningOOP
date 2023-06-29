113ef11799a3c2f08723adecb5af7036868b4b45
VanVT10 <vanminho296@gmail.com>
2023-06-29 17:08:34 +0700

    complete exercise 3

--- src/lesson06/RobotDog.java
+++ src/lesson06/RobotDog.java
@@ -3,9 +3,9 @@ package lesson06;
 public class RobotDog {
 
     private String name;
-    protected static String defaultName; // Don't change => should use "final". Don't have value -> don't have final because can't talk: value of A is nothing.
+    protected static String defaultName;// Don't change => should use "final". Don't have value -> don't have final because can't talk: value of A is nothing.
 
-    RobotDog() {
+    RobotDog(){
 
     }
 
